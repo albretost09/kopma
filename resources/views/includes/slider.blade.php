540be5c0824ec4c6b09c85f4711f9aefdb8781fb
Albreto Suryanata <albretsuryanata@mhs.eng.upr.ac.id>
2023-04-27 02:30:30 +0700

    Update slider.blade.php

--- resources/views/includes/slider.blade.php
+++ resources/views/includes/slider.blade.php
@@ -7,14 +7,6 @@
                 <div class="image-layer"
                     style="background-image:url({{ asset('frontend/assets/images/slides/banner-bg-kopma.jpg') }})">
                 </div>
-                <div class="auto-container">
-                    <div class="content right">
-                        <h2>We Are Most Trusted<br> Immigration Agency</h2>
-                        <div class="btns-box">
-                            <a class="btn-one" href="#tentang-kami"><span class="txt">Discover More</span></a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
