40c09cd3afdd64147ae4f2f416a9f77c419e090f
Albreto Suryanata <albretsuryanata@mhs.eng.upr.ac.id>
2023-05-01 21:42:44 +0700

    Update DashboardController.php

--- app/Http/Controllers/Anggota/Dashboard/DashboardController.php
+++ app/Http/Controllers/Anggota/Dashboard/DashboardController.php
@@ -14,11 +14,28 @@ class DashboardController extends Controller
             ->where('pengguna_id', auth()->user()->id)
             ->where('status', 'DITERIMA')
             ->sum('jumlah') ?? 0;
-        $jumlahSimpananSukarela = Simpanan::query()
+        $simpanan = Simpanan::query()
             ->where('pengguna_id', auth()->user()->id)
             ->where('jenis_simpanan', 'Sukarela')
-            ->where('status', 'DITERIMA')
-            ->sum('jumlah') ?? 0;
+            ->where('status', 'DITERIMA');
+
+        $jumlahSimpanan = $simpanan->sum('jumlah');
+
+        $riwayatPenarikan = $simpanan->with('riwayatPenarikan')->get()->map(function ($item) {
+            return $item->riwayatPenarikan;
+        })->filter(function ($item) {
+            return $item->count() > 0;
+        })->flatten();
+
+        $riwayatPenarikan = $riwayatPenarikan->map(function ($item) {
+            return $item->status != 'DITOLAK' ? $item : null;
+        })->filter(function ($item) {
+            return $item != null;
+        })->flatten();
+
+        $jumlahRiwayatPenarikan = $riwayatPenarikan->sum('jumlah_penarikan');
+
+        $jumlahSimpananSukarela = $jumlahSimpanan - $jumlahRiwayatPenarikan;
         $jumlahSimpananPokokWajib = Simpanan::query()
             ->where('pengguna_id', auth()->user()->id)
             ->where('status', 'DITERIMA')
