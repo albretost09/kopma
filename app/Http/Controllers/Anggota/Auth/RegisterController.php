8f4d723ce8356f3fb9aab017ab4b9700766c7b51
Albreto Suryanata <albretsuryanata@mhs.eng.upr.ac.id>
2023-05-19 07:47:15 +0700

    error handling register

--- app/Http/Controllers/Anggota/Auth/RegisterController.php
+++ app/Http/Controllers/Anggota/Auth/RegisterController.php
@@ -16,21 +16,39 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'nama' => 'required|max:255',
-            'nim' => 'required|unique:pengguna',
-            'username' => 'required|max:255|unique:pengguna',
-            'email' => 'required|email|max:255|unique:pengguna',
-            'no_hp' => 'nullable',
-            'nik' => 'nullable|numeric',
-            'fakultas' => 'nullable',
-            'jurusan' => 'nullable',
-            'tempat_lahir' => 'nullable',
-            'tanggal_lahir' => 'nullable|date',
-            'jenis_kelamin' => 'nullable',
-            'alamat' => 'nullable',
-            'password' => 'required|confirmed|min:8',
-        ]);
+        $request->validate(
+            [
+                'nama' => 'required|max:255',
+                'nim' => 'required|unique:pengguna',
+                'username' => 'required|max:255|unique:pengguna',
+                'email' => 'required|email|max:255|unique:pengguna',
+                'no_hp' => 'nullable',
+                'nik' => 'nullable|numeric',
+                'fakultas' => 'nullable',
+                'jurusan' => 'nullable',
+                'tempat_lahir' => 'nullable',
+                'tanggal_lahir' => 'nullable|date',
+                'jenis_kelamin' => 'nullable',
+                'alamat' => 'nullable',
+                'password' => 'required|confirmed|min:8',
+            ],
+            [
+                'nama.required' => 'Nama harus diisi.',
+                'nim.required' => 'NIM harus diisi.',
+                'nim.unique' => 'NIM sudah terdaftar.',
+                'username.required' => 'Username harus diisi.',
+                'username.unique' => 'Username sudah terdaftar.',
+                'email.required' => 'Email harus diisi.',
+                'email.email' => 'Email tidak valid.',
+                'email.unique' => 'Email sudah terdaftar.',
+                'no_hp.numeric' => 'No. HP harus berupa angka.',
+                'nik.numeric' => 'NIK harus berupa angka.',
+                'tanggal_lahir.date' => 'Tanggal lahir harus berupa tanggal.',
+                'password.required' => 'Password harus diisi.',
+                'password.confirmed' => 'Password tidak sama.',
+                'password.min' => 'Password minimal 8 karakter.',
+            ]
+        );
 
         if ($request->password != $request->password_confirmation) {
             return redirect()->route('anggota.auth.register')->with('error', 'Password tidak sama.');
--- resources/views/pages/anggota/auth/register.blade.php
+++ resources/views/pages/anggota/auth/register.blade.php
@@ -15,68 +15,93 @@
         <!-- form -->
         <form action="{{ route('register.store') }}" method="POST">
             @csrf
-            <div class="row">
+            <div class="row text-left">
                 <div class="col-md-6">
                     <div class="form-group">
-                        <input type="text" class="form-control" name="nama" placeholder="Nama">
+                        <input type="text" class="form-control mb-1" name="nama" placeholder="Nama"
+                            value="{{ old('nama') }}">
+                        <small class="text-danger">{{ $errors->first('nama') }}</small>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="nim" placeholder="NIM">
+                        <input type="text" class="form-control mb-1" name="nim" placeholder="NIM"
+                            value="{{ old('nim') }}">
+                        <small class="text-danger">{{ $errors->first('nim') }}</small>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="username" placeholder="Username">
+                        <input type="text" class="form-control mb-1" name="username" placeholder="Username"
+                            value="{{ old('username') }}">
+                        <small class="text-danger">{{ $errors->first('username') }}</small>
                     </div>
                     <div class="form-group">
-                        <input type="email" class="form-control" name="email" placeholder="Email">
+                        <input type="email" class="form-control mb-1" name="email" placeholder="Email"
+                            value="{{ old('email') }}">
+                        <small class="text-danger">{{ $errors->first('email') }}</small>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="no_hp" placeholder="No. HP">
+                        <input type="text" class="form-control mb-1" name="no_hp" placeholder="No. HP"
+                            value="{{ old('no_hp') }}">
+                        <small class="text-danger">{{ $errors->first('no_hp') }}</small>
                     </div>
                     <div class="form-group">
-                        <input type="number" class="form-control" name="nik" placeholder="NIK">
+                        <input type="text" class="form-control mb-1" name="nik" placeholder="NIK"
+                            value="{{ old('nik') }}">
+                        <small class="text-danger">{{ $errors->first('nik') }}</small>
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <div class="form-group">
-                        <select class="form-control select2" name="fakultas">
-                            <option value="">Fakultas</option>
-                            <option value="FKIP">FKIP</option>
-                            <option value="Ekonomi">Ekonomi</option>
-                            <option value="Pertanian">Pertanian</option>
-                            <option value="Teknik">Teknik</option>
-                            <option value="Hukum">Hukum</option>
-                            <option value="FISIP">FISIP</option>
-                            <option value="Dokter">Dokter</option>
-                            <option value="MIPA">MIPA</option>
+                    <div class="form-group text-left">
+                        <select class="form-control mb-1 select2" name="fakultas">
+                            <option value="" {{ old('fakultas') == '' ? 'selected' : '' }}>Fakultas</option>
+                            <option value="FKIP" {{ old('fakultas') == 'FKIP' ? 'selected' : '' }}>FKIP</option>
+                            <option value="Ekonomi" {{ old('fakultas') == 'Ekonomi' ? 'selected' : '' }}>Ekonomi
+                            <option value="Pertanian" {{ old('fakultas') == 'Pertanian' ? 'selected' : '' }}>Pertanian
+                            <option value="Teknik" {{ old('fakultas') == 'Teknik' ? 'selected' : '' }}>Teknik</option>
+                            <option value="Hukum" {{ old('fakultas') == 'Hukum' ? 'selected' : '' }}>Hukum</option>
+                            <option value="FISIP" {{ old('fakultas') == 'FISIP' ? 'selected' : '' }}>FISIP</option>
+                            <option value="Dokter" {{ old('fakultas') == 'Dokter' ? 'selected' : '' }}>Dokter</option>
+                            <option value="MIPA" {{ old('fakultas') == 'MIPA' ? 'selected' : '' }}>MIPA</option>
                         </select>
+                        <small class="text-danger">{{ $errors->first('fakultas') }}</small>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="jurusan" placeholder="Jurusan">
+                        <input type="text" class="form-control mb-1" name="jurusan" placeholder="Jurusan"
+                            value="{{ old('jurusan') }}">
+                        <small class="text-danger">{{ $errors->first('jurusan') }}</small>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="tempat_lahir" placeholder="Tempat Lahir">
+                        <input type="text" class="form-control mb-1" name="tempat_lahir" placeholder="Tempat Lahir"
+                            value="{{ old('tempat_lahir') }}">
+                        <small class="text-danger">{{ $errors->first('tempat_lahir') }}</small>
                     </div>
                     <div class="form-group">
-                        <input type="text" id="daterangepicker" name="tanggal_lahir" class="form-control">
+                        <input type="text" id="daterangepicker" name="tanggal_lahir" class="form-control mb-1"
+                            value="{{ old('tanggal_lahir') }}" placeholder="Tanggal Lahir">
+                        <small class="text-danger">{{ $errors->first('tanggal_lahir') }}</small>
                     </div>
-                    <div class="form-group">
-                        <select class="form-control select2" name="jenis_kelamin">
-                            <option value="">Jenis Kelamin</option>
-                            <option value="L">Laki-laki</option>
-                            <option value="P">Perempuan</option>
+                    <div class="form-group text-left">
+                        <select class="form-control mb-1 select2" name="jenis_kelamin">
+                            <option value="" {{ old('jenis_kelamin') == '' ? 'selected' : '' }}>Jenis Kelamin
+                            </option>
+                            <option value="L" {{ old('jenis_kelamin') == 'L' ? 'selected' : '' }}>Laki-laki</option>
+                            <option value="P" {{ old('jenis_kelamin') == 'P' ? 'selected' : '' }}>Perempuan</option>
                         </select>
+                        <small class="text-danger">{{ $errors->first('jenis_kelamin') }}</small>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="alamat" placeholder="Alamat">
+                        <input type="text" class="form-control mb-1" name="alamat" placeholder="Alamat"
+                            value="{{ old('alamat') }}">
+                        <small class="text-danger">{{ $errors->first('alamat') }}</small>
                     </div>
                 </div>
                 <div class="col-md-12">
                     <div class="form-group">
-                        <input type="password" class="form-control" name="password" placeholder="Password">
+                        <input type="password" class="form-control mb-1" name="password" placeholder="Password"
+                            value="{{ old('password') }}">
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" name="password_confirmation"
-                            placeholder="Confirmation Password">
+                        <input type="password" class="form-control mb-1" name="password_confirmation"
+                            placeholder="Confirmation Password" value="{{ old('password_confirmation') }}">
+                        <small class="text-danger">{{ $errors->first('password_confirmation') }}</small>
                     </div>
                 </div>
             </div>
@@ -95,12 +120,27 @@
 
 @push('script')
     <script src="{{ asset('backend/vendors/datepicker/daterangepicker.js') }}"></script>
+    <script src="{{ asset('backend/vendors/input-mask/jquery.mask.js') }}"></script>
     <script>
         $(document).ready(function() {
             $('#daterangepicker').daterangepicker({
                 singleDatePicker: true,
                 showDropdowns: true
             });
+
+            $('input[name="nim"]').on('keyup', function() {
+                var nim = $(this).val().replace(/\s/g, '');
+                $(this).val(nim);
+            });
+
+            $('input[name="no_hp"]').mask('+62 999999999999');
+
+            $('input[name="nik"]').on('keyup', function() {
+                var nik = $(this).val().replace(/\s/g, '');
+                $(this).val(nik);
+            });
+
+            $('input[name="nik"]').mask('9999999999999999');
 
             toastr.options = {
                 timeOut: 3000,
@@ -117,10 +157,6 @@
 
             @if (session()->has('error'))
                 toastr.danger("{{ session('error') }}");
-            @endif
-
-            @if (session()->has('errors'))
-                toastr.error("{{ session('errors')->first('username') }}");
             @endif
         });
     </script>
