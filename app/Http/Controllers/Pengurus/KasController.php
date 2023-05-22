1badb6da39db6a133d5220f7b1100ff651773675
Albreto Suryanata <albretsuryanata@mhs.eng.upr.ac.id>
2023-05-22 09:47:26 +0700

    udpate kas

--- app/Http/Controllers/Pengurus/KasController.php
+++ app/Http/Controllers/Pengurus/KasController.php
@@ -115,7 +115,11 @@ class KasController extends Controller
         $no_cek = $nomor . '/' . $jenis . '/' . $bulan . '/' . $tahun;
 
         if (Kas::where('no_cek', $no_cek)->exists()) {
-            $nomor = $nomor + 1;
+            $bulan = explode('/', $data['tanggal_transaksi'])[1];
+            $tahun = explode('/', $data['tanggal_transaksi'])[2];
+            $nomor = Kas::whereMonth('tanggal_transaksi', $bulan)->whereYear('tanggal_transaksi', $tahun)->where('jenis', $data['jenis'])->exists() ? Kas::whereMonth('tanggal_transaksi', $bulan)->whereYear('tanggal_transaksi', $tahun)->where('jenis', $data['jenis'])->latest()->first()->id + 1 : 1;
+            $nomor = ($nomor + 1);
+            $nomor = str_pad($nomor, 3, '0', STR_PAD_LEFT);
             // convert to romawi
             $bulan = toRoman($bulan);
             $tahun = explode('/', $data['tanggal_transaksi'])[2];
